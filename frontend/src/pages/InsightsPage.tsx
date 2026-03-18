8ae058299c055c0e3d2223d041901ac55649d122
Donghee <donghee1358@gmail.com>
2026-03-18 16:36:42 +0900

    import 추가

--- frontend/src/pages/InsightsPage.tsx
+++ frontend/src/pages/InsightsPage.tsx
@@ -33,6 +33,10 @@ import {
   Cell,
 } from 'recharts';
 import { formatCurrency, formatCompactNumber, getPlatformColor } from '@/lib/utils';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+
+
 
 const TOUR_STEPS = [
   {
